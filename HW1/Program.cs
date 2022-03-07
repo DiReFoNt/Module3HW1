14f7a6677bcb31463aec42a245dc145299952625
Alexandr Basiuk <basuksasa@gmail.com>
2022-03-07 12:12:02 +0200

    Successful complete task

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -2,11 +2,29 @@
 
 namespace HW1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
+            int[] test = new int[] { 1, 28, 31, 3, 11, 2, 19, 34 };
+            Generic<int> generic = new Generic<int>();
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
 
+            test = generic.AddRange(test);
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+
+            Console.WriteLine("===========");
+            generic.Sort();
+            for (int i = 0; i < generic.Count; i++)
+            {
+                Console.WriteLine(generic[i]);
+            }
         }
     }
 }
